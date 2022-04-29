ce114875457476e0920c4abdd0bd2671871eac70
priyankapeelu <priyankapeelu@icloud.com>
2022-04-29 15:12:36 +0530

    terraform variablescd tt

--- cluster.tf
+++ cluster.tf
@@ -1,13 +1,12 @@
-#resource "aws_docdb_cluster" "docdb" {
-#  cluster_identifier = "roboshop-${var.ENV}"
-#  engine             = "docdb"
-#  master_username    = "admin1"
-#  master_password    = "roboshop1"
-#  ## This is just for lab purpose
-#  skip_final_snapshot    = true
-#  db_subnet_group_name   = aws_docdb_subnet_group.docdb.name
-#  vpc_security_group_ids = [aws_security_group.allow_mongodb.id]
-#}
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier   = "roboshop-${var.ENV}"
+  engine               = "docdb"
+  master_username      = "admin1"
+  master_password      = "roboshop1"
+  ## This is just for lab purpose
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_docdb_subnet_group.docdb.name
+}
 
 
 resource "aws_docdb_subnet_group" "docdb" {
