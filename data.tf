90a1f6cc5ccc6319c7896586e90c3467302f3278
priyankapeelu <priyankapeelu@icloud.com>
2022-06-06 18:00:06 +0530

    terraform variablescd tt

--- data.tf
+++ data.tf
@@ -12,5 +12,5 @@ data "aws_secretsmanager_secret" "secrets" {
 }
 
 data "aws_secretsmanager_secret_version" "secrets" {
-  secret_id = data.aws_secretmanager_secret.secrets.id
+  secret_id = data.aws_secretsmanager_secret.secrets.id
 }
